662d0339bfd8733ede2511ba7d082c221d0ec37d
SalahEddineGhamri <salaheddine.ghamri@yahoo.com>
2020-03-08 13:00:16 +0100

    Rectified

--- code.py
+++ code.py
@@ -3,7 +3,4 @@ print('Initial file')
 print('this is coming from b2')
 print('This is coming from branch b1')
 print('this is coming from branch c of b1')
-<<<<<<< HEAD
-=======
 print('cool')
->>>>>>> cd9f12b
